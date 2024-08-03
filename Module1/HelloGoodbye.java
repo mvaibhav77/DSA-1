64e8d24771d55a94285ceaaa388f4090f8a01d68
Vaibhav Shukla <shukla.vaibhav1077@gmail.com>
2024-08-03 17:01:33 +0530

    learn: stacks and queues implementations

--- Module1/HelloGoodbye.java
+++ Module1/HelloGoodbye.java
@@ -1,10 +1,12 @@
 
 public class HelloGoodbye {
   public static void main(String[] args) {
-      String s = "";
-      for(int i=0; i<args.length; i++) {
-          s += args[i] + " ";
+      if(args.length < 2) {
+        System.out.println("Please give name inputs...");
+        return;
       }
+      
+      String s = args[0] + " " + args[1];
 
       if(!s.equals("")) {
         System.out.println("Hello " + s);
